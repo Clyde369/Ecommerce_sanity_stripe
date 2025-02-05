3231bbc6b8040168ef78ca8ad606bc6432d1086f
Clyde Apples <clyde.apples@gmail.com>
2025-02-05 17:56:56 +0200

    LIVE

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,25 +1,49 @@
+import Link from "next/link";
 
-import Link from 'next/link'
+import { urlFor } from "../lib/client";
 
-import { urlFor } from '../lib/client'
-
-const Product = ({product: {image, name, slug, price}}) => {
-  return (
-    <div>
-      <Link href={`/product/${slug.current}`}>
-        <div className="product-card">
-          <img src={urlFor(image && image[0])}
-          alt=""
-            width={250}
-            height={250}
-            className="product-image"
-          />
-          <p className="product-name">{name}</p>
-          <p className="product-price">R{price}</p>
+const Product = ({
+    product: {
+        image,
+        name,
+        slug,
+        price,
+    },
+}) => {
+    console.log(image);
+    console.log(name);
+    console.log(slug);
+    console.log(price);
+    return (
+        <div>
+            <Link
+                href={`/product/${slug?.current}`}
+            >
+                <div className="product-card">
+                    <img
+                        src={
+                            image &&
+                            image[0]
+                                ? urlFor(
+                                      image[0]
+                                  )
+                                : ""
+                        }
+                        alt={name || ""}
+                        width={250}
+                        height={250}
+                        className="product-image"
+                    />
+                    <p className="product-name">
+                        {name}
+                    </p>
+                    <p className="product-price">
+                        R{price}
+                    </p>
+                </div>
+            </Link>
         </div>
-      </Link>
-    </div>
-  )
-}
+    );
+};
 
-export default Product
+export default Product;
